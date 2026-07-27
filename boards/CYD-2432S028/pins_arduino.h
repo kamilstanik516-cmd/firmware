f1e2cf9ab4d77332963d40021ee1ba4a7fb9afa5
kamilstanik516-cmd <kamilstanik516@gmail.com>
2026-07-27 15:58:34 +0200

    Update pin definitions in pins_arduino.h
    Removed SDA and SCL pin definitions and added CS, DC, and RST pin definitions.

--- boards/CYD-2432S028/pins_arduino.h
+++ boards/CYD-2432S028/pins_arduino.h
@@ -6,8 +6,7 @@
 static const uint8_t TX = 1;
 static const uint8_t RX = 3;
 
-static const uint8_t SDA = 21;
-static const uint8_t SCL = 22;
+
 
 static const uint8_t CS 17
 static const uint8_t DC 16
